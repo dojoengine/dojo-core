9019ecfb1537518a697d43f00eff36326b6799f2
glihm <dev@glihm.net>
2024-10-08 22:38:20 -0600

    fix: update tests script

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -2,22 +2,22 @@
 # To output the Cairo fix tests, use the `--fix` argument.
 
 # Testing the dojo compiler.
-cargo test -p dojo-compiler
+cargo build -r --workspace
+
+# Testing with the demo compiler.
+if [ "$1" == "--fix" ]; then
+    CAIRO_FIX_TESTS=1 cargo test -p dojo-compiler
+else
+    cargo test -p dojo-compiler
+fi
 
 # Testing abigen content, which also builds the core contracts.
 # Need to check because the formatting of the generated code is then different from the one
 # generated on the fly.
 if [ "$1" == "--fix" ]; then
-    cargo run -r --bin dojo-abigen
+    ./target/release/dojo-abigen
 else
-    cargo run -r --bin dojo-abigen -- --check
-fi
-
-# Testing with the demo compiler.
-if [ "$1" == "--fix" ]; then
-    CAIRO_FIX_TESTS=1 cargo build -r --bin demo-compiler
-else
-    cargo build -r --bin demo-compiler
+    ./target/release/dojo-abigen --check
 fi
 
 ./target/release/demo-compiler test --manifest-path crates/contracts/Scarb.toml
